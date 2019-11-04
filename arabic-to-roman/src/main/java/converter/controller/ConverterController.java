3222f67c5c9f430cd78bd47d4addd749a9ebb7a4
laura Giraldo <laura.giraldo@quind.io>
2019-11-04 17:56:21 -0500

    se modifica controller para retornar numero romano

--- arabic-to-roman/src/main/java/converter/controller/ConverterController.java
+++ arabic-to-roman/src/main/java/converter/controller/ConverterController.java
@@ -1,23 +1,22 @@
 package converter.controller;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import converter.domain.*;
 
-@Controller
+@RestController
+@RequestMapping("/converter")
 public class ConverterController {
 protected ObjectMapper mapper;
 	
-	@GetMapping("/converter")
+	@GetMapping
 	public String converterToRoman( @RequestParam double num) {
 		return  Converter.converterToRoman(num);
 	}
-	 @RequestMapping("/")
-	    public String index() {
-	        return "Hello Basic Calculator!";
-	    }
 }
