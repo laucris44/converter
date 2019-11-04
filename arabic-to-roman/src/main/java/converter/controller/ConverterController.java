8db8e6c754893298b0a71d8210ba475f6382479d
laura Giraldo <laura.giraldo@quind.io>
2019-11-04 16:44:01 -0500

    se modifica Jenkinsfile

--- arabic-to-roman/src/main/java/converter/controller/ConverterController.java
+++ arabic-to-roman/src/main/java/converter/controller/ConverterController.java
@@ -3,9 +3,7 @@ package converter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import converter.domain.*;
