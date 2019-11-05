e05e53aab02330910e628876f8f307572aaff014
laura Giraldo <laura.giraldo@quind.io>
2019-11-04 22:22:31 -0500

    se adiciona mensaje index

--- arabic-to-roman/src/main/java/converter/controller/ConverterController.java
+++ arabic-to-roman/src/main/java/converter/controller/ConverterController.java
@@ -1,21 +1,25 @@
 package converter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import converter.domain.*;
 
 @RestController
-@RequestMapping("/converter")
 public class ConverterController {
-protected ObjectMapper mapper;
-	
+	protected ObjectMapper mapper;
+
+	@RequestMapping("/converter/{num}")
 	@GetMapping
-	public String converterToRoman( @RequestParam double num) {
+	public String converterToRoman( @PathVariable double num) {
 		return  Converter.converterToRoman(num);
 	}
+	
+	@RequestMapping
+    public String index() {
+        return "Conversor numeros arabigos a romanos. Ingrese el numero entero (entre 1 y 999) que desea convertir a romano con el siguiente query: /converter/{num}";
+    }
 }
